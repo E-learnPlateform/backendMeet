78bbceb65f564dca2d6fddc4df7776466f5a9c2a
E-learnPlateform <147305034+E-learnPlateform@users.noreply.github.com>
2024-10-13 11:39:04 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,53 +1,44 @@
-const express = require("express");
-const http = require("http");
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
 const app = express();
 const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server,{
-     origin:"*"
-});
+const io = socketIo(server, { cors: { origin: '*' } });
 
-const users = {};
+app.use(cors());
+app.use(express.json());
 
-const socketToRoom = {};
+const rooms = {};
 
-io.on('connection', socket => {
-    socket.on("join room", roomID => {
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit("room full");
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
+io.on('connection', (socket) => {
+    console.log('New user connected:', socket.id);
+
+    socket.on('joinRoom', ({ roomId, peerId }) => {
+        socket.join(roomId);
+        if (!rooms[roomId]) {
+            rooms[roomId] = [];
         }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-
-        socket.emit("all users", usersInThisRoom);
+        rooms[roomId].push(peerId);
+        socket.to(roomId).emit('newPeer', peerId);
     });
 
-    socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
-
-    socket.on("returning signal", payload => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    socket.on('chatMessage', (messageData) => {
+        const { peerId, text } = messageData;
+        socket.to(roomId).emit('chatMessage', { peerId, text });
     });
 
     socket.on('disconnect', () => {
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter(id => id !== socket.id);
-            users[roomID] = room;
+        for (const roomId in rooms) {
+            rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+            socket.to(roomId).emit('peerLeft', socket.id);
         }
     });
-
 });
 
-server.listen(3001, () => console.log('server is running on port 3001'));
-
-
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
